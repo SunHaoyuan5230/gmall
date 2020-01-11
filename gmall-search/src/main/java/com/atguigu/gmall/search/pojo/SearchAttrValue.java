b003a9a19833847219a795772453e8c564008793
孙豪远 <yuan5230@outlook.com>
2020-01-11 08:38:05 +0800

    day07_搜索工程的搭建ä»&完成数据模型的设计&数据导入

---
+++ gmall-search/src/main/java/com/atguigu/gmall/search/pojo/SearchAttrValue.java
@@ -0,0 +1,9 @@
+package com.atguigu.gmall.search.pojo;
+
+/**
+ * @Auther: SunHaoyuan
+ * @Date: 2020/1/9 10:46
+ * @Description:
+ */
+public class SearchAttrValue {
+}
---
+++ gmall-search/src/main/java/com/atguigu/gmall/search/respository/GoodsRepository.java
@@ -0,0 +1,9 @@
+package com.atguigu.gmall.search.respository;
+
+/**
+ * @Auther: SunHaoyuan
+ * @Date: 2020/1/9 16:26
+ * @Description:
+ */
+public class GoodsRepository {
+}
